53521bcb0f0cc4de54012de5ac71236c578a6fe7
duinoprojects <50635573+duinoprojects@users.noreply.github.com>
2019-05-14 23:41:44 -0700

    Part 01,02

---
+++ Blink01.ino
@@ -0,0 +1,22 @@
+ int delay_time=1000;
+
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(9, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  blink_ligt(5); 
+}
+
+void blink_ligt(int light_pin){
+  digitalWrite(light_pin, HIGH);         // turn the LED on (HIGH is the voltage level)
+  delay(delay_time);                     // wait for a second
+  digitalWrite(light_pin, LOW);          // turn the LED off by making the voltage LOW
+  delay(delay_time);                     // wait for a second 
+}
